8e973f655cda47abbbc54a8a2f093a19db59e7e9
mtkessel <the.rocketredneck@gmail.com>
2018-11-04 06:09:25 -0700

    Initial Commit for Example Bot

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,25 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+
+import frc.robot.IdGroup;
+
+public class RobotMap
+{
+    public static int DRIVER_JOYSTICK_ID = 0;
+
+    public static int CAN_TIMEOUT_MSEC = 10;    // Only use this during initialization; use zero for periodic calls
+
+    public static int DRIVE_MOTOR_LEFT_A_ID  = 11;
+    public static int DRIVE_MOTOR_LEFT_B_ID  = 12;
+    public static int DRIVE_MOTOR_RIGHT_A_ID = 13;
+    public static int DRIVE_MOTOR_RIGHT_B_ID = 14;
+
+    final public static IdGroup DRIVE_MOTOR_LEFT_ID_GROUP = new IdGroup(DRIVE_MOTOR_LEFT_A_ID,
+                                                                        DRIVE_MOTOR_LEFT_B_ID);
+
+    final public static IdGroup DRIVE_MOTOR_RIGHT_ID_GROUP = new IdGroup(DRIVE_MOTOR_RIGHT_A_ID,
+                                                                         DRIVE_MOTOR_RIGHT_B_ID);
+
+    final public static FeedbackDevice DRIVE_MOTOR_FEEDBACK_DEVICE = FeedbackDevice.QuadEncoder;
+};
---
+++ src/main/java/frc/robot/drivesubsystem/DriveSubsystem.java
@@ -0,0 +1,375 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.drivesubsystem;
+
+import frc.robot.IdGroup;
+
+import com.ctre.phoenix.motorcontrol.ControlFrame;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
+import com.ctre.phoenix.motorcontrol.LimitSwitchSource;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.RemoteSensorSource;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.ErrorCode;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * Add your docs here.
+ */
+public class DriveSubsystem extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    private WPI_TalonSRX[] leftMotors;
+    private WPI_TalonSRX[] rightMotors;
+
+    private double[] leftCurrent_amp;
+    private double[] rightCurrent_amp;
+
+    private double leftPosition_ticks;
+    private double rightPosition_ticks;
+
+    private double leftVelocity_ticksPsec;
+    private double rightVelocity_ticksPsec;
+
+
+    private final FeedbackDevice FEEDBACK_DEVICE;
+    private final int CAN_TIMEOUT_MSEC;
+
+    private DifferentialDrive drive;
+
+    private enum DriveStyles 
+    {
+        WPI_ARCADE, 
+        WPI_CURVATURE, 
+        WPI_TANK, 
+        MODIFIED_ARCADE,
+    };
+
+    private SendableChooser<DriveStyles> driveStyleChooser;
+
+    private boolean telemetryEnabled;
+
+    /***
+     * Constructor
+     */
+    public DriveSubsystem(IdGroup leftIds, 
+                          IdGroup rightIds, 
+                          FeedbackDevice aFeedbackDevice,
+                          int aTimeout_msec) 
+    {
+        driveStyleChooser = new SendableChooser<DriveStyles>(); // put this on the dashboard at initialization
+        driveStyleChooser.addDefault("WPI Arcade",      DriveStyles.WPI_ARCADE);
+        driveStyleChooser.addObject( "WPI Curvature",   DriveStyles.WPI_CURVATURE);
+        driveStyleChooser.addObject( "WPI Tank",        DriveStyles.WPI_TANK);
+        driveStyleChooser.addObject( "Modified Arcade", DriveStyles.MODIFIED_ARCADE);
+
+        // Configure motors at initialiation
+        leftMotors = new WPI_TalonSRX[leftIds.length()];
+        rightMotors = new WPI_TalonSRX[rightIds.length()];
+
+        leftCurrent_amp = new double[leftMotors.length];
+        rightCurrent_amp = new double[rightMotors.length];
+
+        for (int i = 0; i < leftMotors.length; ++i) 
+        {
+            leftMotors[i] = new WPI_TalonSRX(leftIds.get(i));
+            leftMotors[i].setName(getName(),"Left_" + Integer.toString(i));            
+        }
+        for (int i = 0; i < rightMotors.length; ++i) 
+        {
+            rightMotors[i] = new WPI_TalonSRX(rightIds.get(i));
+            rightMotors[i].setName(getName(),"Right_" + Integer.toString(i));            
+        }
+
+        FEEDBACK_DEVICE = aFeedbackDevice;
+        CAN_TIMEOUT_MSEC = aTimeout_msec;
+
+        // All other motors will be followers so we only need to initialize the drive
+        // with the first motor in each group
+        // NOTE: We don't use the SpeedControllerGroup because the behavior requires
+        // additional CAN traffic that is not needed with TalonSRX controllers.
+        drive = new DifferentialDrive(leftMotors[0], rightMotors[0]);
+
+        telemetryEnabled = false;
+    }
+
+    /**
+     * initializeMotor - set all of the motor configuration states to a known value
+     * This is important when we are not sure if the motor is in a factory state
+     * @param aMotor
+     * 
+     * TODO: Move this to a separate package?
+     */
+    private void initializeMotor(WPI_TalonSRX aMotor)
+    {
+        // TODO: Check ErrorCode?
+
+        aMotor.stopMotor();
+
+        // Group all of the slot configurations together for a loop
+        for (int slotIdx = 0; slotIdx < 2; ++slotIdx)   // TODO: Source MAGIC NUMBER 2, what is the actual number of slots in the firmware?
+        {
+            aMotor.config_IntegralZone(slotIdx, 0, CAN_TIMEOUT_MSEC);
+            aMotor.config_kD(slotIdx, 0.0, CAN_TIMEOUT_MSEC);
+            aMotor.config_kF(slotIdx, 0.0, CAN_TIMEOUT_MSEC);
+            aMotor.config_kI(slotIdx, 0.0, CAN_TIMEOUT_MSEC);
+            aMotor.config_kP(slotIdx, 0.0, CAN_TIMEOUT_MSEC);
+            aMotor.configClosedLoopPeakOutput(slotIdx, 1.0, CAN_TIMEOUT_MSEC);
+            aMotor.configClosedLoopPeriod(slotIdx, 1, CAN_TIMEOUT_MSEC);
+            aMotor.configMaxIntegralAccumulator(slotIdx, 0, CAN_TIMEOUT_MSEC);
+            aMotor.configAllowableClosedloopError(slotIdx, 0, CAN_TIMEOUT_MSEC);
+        }
+
+        aMotor.configAuxPIDPolarity(false, CAN_TIMEOUT_MSEC);
+
+        aMotor.configOpenloopRamp(0.0, CAN_TIMEOUT_MSEC);
+        aMotor.configClosedloopRamp(0.0, CAN_TIMEOUT_MSEC);
+
+        // Start with current limiting disabled and set to a large value
+        aMotor.enableCurrentLimit(false);
+        aMotor.configContinuousCurrentLimit(90, CAN_TIMEOUT_MSEC);
+        aMotor.configPeakCurrentDuration(0, CAN_TIMEOUT_MSEC);
+        aMotor.configPeakCurrentLimit(0, CAN_TIMEOUT_MSEC);
+
+        // Assume we have no limit switches associated with this motor
+        aMotor.configForwardLimitSwitchSource(LimitSwitchSource.Deactivated, LimitSwitchNormal.Disabled, CAN_TIMEOUT_MSEC);
+        aMotor.configForwardSoftLimitEnable(false, CAN_TIMEOUT_MSEC);
+        aMotor.configForwardSoftLimitThreshold(0, CAN_TIMEOUT_MSEC);
+        aMotor.configReverseLimitSwitchSource(LimitSwitchSource.Deactivated, LimitSwitchNormal.Disabled, CAN_TIMEOUT_MSEC);
+        aMotor.configReverseSoftLimitEnable(false, CAN_TIMEOUT_MSEC);
+        aMotor.configReverseSoftLimitThreshold(0, CAN_TIMEOUT_MSEC);
+
+        // Disable motion magic and profile features until we know we are going to use them
+        aMotor.configMotionAcceleration(0, CAN_TIMEOUT_MSEC);
+        aMotor.configMotionCruiseVelocity(0, CAN_TIMEOUT_MSEC);
+        aMotor.configMotionProfileTrajectoryPeriod(0, CAN_TIMEOUT_MSEC);
+
+        // Reinforce factor default deadband and minimum output
+        aMotor.configNeutralDeadband(0.04, CAN_TIMEOUT_MSEC);
+        aMotor.configNominalOutputForward(0.0, CAN_TIMEOUT_MSEC);
+        aMotor.configNominalOutputReverse(0.0, CAN_TIMEOUT_MSEC);
+
+        aMotor.configPeakOutputForward(1.0, CAN_TIMEOUT_MSEC);
+        aMotor.configPeakOutputReverse(1.0, CAN_TIMEOUT_MSEC);
+
+        // Feedback sources, start with no local and no remote sensors
+        // NOTE: Remote sensors can be things like the Pigeon IMU
+        aMotor.configRemoteFeedbackFilter(0, RemoteSensorSource.Off, 0, CAN_TIMEOUT_MSEC);        
+        for (int pidIdx = 0; pidIdx <= 1; ++pidIdx)
+        {
+            aMotor.configSelectedFeedbackCoefficient(1.0, pidIdx, CAN_TIMEOUT_MSEC);
+            aMotor.configSelectedFeedbackSensor(FeedbackDevice.None, pidIdx, CAN_TIMEOUT_MSEC);
+
+            aMotor.setIntegralAccumulator(0, pidIdx, CAN_TIMEOUT_MSEC);
+            aMotor.setSelectedSensorPosition(0, pidIdx, CAN_TIMEOUT_MSEC);
+        }
+
+        // TODO: Figure out what these do!!!!
+        //aMotor.configSensorTerm(sensorTerm, feedbackDevice, CAN_TIMEOUT_MSEC);
+        //aMotor.configVelocityMeasurementPeriod(period, CAN_TIMEOUT_MSEC);
+        //aMotor.configVelocityMeasurementWindow(windowSize, CAN_TIMEOUT_MSEC);
+
+        aMotor.enableVoltageCompensation(false);
+        aMotor.configVoltageCompSaturation(12.0, CAN_TIMEOUT_MSEC);
+        aMotor.configVoltageMeasurementFilter(32, CAN_TIMEOUT_MSEC);  // Default
+        
+        // Reinforce control frame period defaults
+        // NOTE: Decreasing general control frame period to 1 ms will increase CAN traffic by about 15%
+        aMotor.setControlFramePeriod(ControlFrame.Control_3_General, 10);
+        // TODO: aMotor.setControlFramePeriod(ControlFrame.Control_4_Advanced, 10);
+        aMotor.setControlFramePeriod(ControlFrame.Control_6_MotProfAddTrajPoint, 10);
+
+
+        aMotor.setNeutralMode(NeutralMode.Brake);
+
+        aMotor.setSafetyEnabled(false);
+        aMotor.setExpiration(0.500);
+
+        aMotor.setInverted(false);
+        aMotor.setSensorPhase(false);
+
+        // Reinforce the factory defaults that we might change later
+        // TODO: To improve CAN bus utilization consider setting the unused one to even lower rates (longer periods)
+        aMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_1_General,        10, CAN_TIMEOUT_MSEC);
+        aMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0,      20, CAN_TIMEOUT_MSEC);
+        aMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_3_Quadrature,    160, CAN_TIMEOUT_MSEC);
+        aMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_4_AinTempVbat,   160, CAN_TIMEOUT_MSEC);
+        aMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_8_PulseWidth,    160, CAN_TIMEOUT_MSEC);
+        aMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic,  160, CAN_TIMEOUT_MSEC);
+        aMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0,   160, CAN_TIMEOUT_MSEC);
+
+        aMotor.selectDemandType(false);   // Future feature
+
+        // TODO: Read faults before clearing?
+        aMotor.clearMotionProfileHasUnderrun(CAN_TIMEOUT_MSEC);
+        aMotor.clearStickyFaults(CAN_TIMEOUT_MSEC);
+
+        aMotor.changeMotionControlFramePeriod(25);  // Assume that any motion profile will step at 50 ms
+        aMotor.clearMotionProfileTrajectories();
+        aMotor.enableHeadingHold(false); // Future feature
+
+        // Use constants from slot 0 and loop 0 (i.e., primary PID) for basic closed loop control
+        // See SRM Section 9.10 for more information about the Auxilliary loop
+        aMotor.selectProfileSlot(0, 0);
+
+    }
+    /***
+     * initialize - performs all post construction initialization This is necessary
+     * because some operations cannot be performed until the entire robot has been
+     * constructed
+     * 
+     * Call this function once in robotInit
+     */
+    public void initialize() 
+    {
+        // Set up all of the initial smartdashboard items in one place so they are
+        // easier to find. Some will only be generated when telemetry is enabled
+        // and others are generated in periodic cycles, so these items are the
+        // ones that are needed immediately at initialization because they are
+        // generated once
+        SmartDashboard.putData("DriveStyles", driveStyleChooser);
+        SmartDashboard.putBoolean("DriveTelemetryEnabled", telemetryEnabled);
+
+        // Set up follower commands only once to minimize CAN traffic
+        for (int i = 1; i < leftMotors.length; ++i) 
+        {
+            leftMotors[i].follow(leftMotors[0]);
+        }
+        for (int i = 1; i < rightMotors.length; ++i) 
+        {
+            rightMotors[i].follow(rightMotors[0]);
+        }
+
+        // Loop through each motor to configure every setting for deterministic
+        // behavior.
+        // NOTE: During development it is essential that all settings are pushed
+        // to avoid unexpected behaviors. If we were producing a final product
+        // to be delivered we would design the system with a separate programming
+        // state to push the values; this would save boot time and wear-and-tear
+        // of the flash memory
+        for (int i = 0; i < leftMotors.length; ++i)
+        {
+            initializeMotor(leftMotors[i]);
+        }
+
+        // Configure primary motor sensor device for all feedback loops
+        for (int pidIndex = 0; pidIndex <= 1; ++pidIndex)
+        {
+            ErrorCode errorCode = leftMotors[0].configSelectedFeedbackSensor(FEEDBACK_DEVICE, pidIndex, CAN_TIMEOUT_MSEC);
+            if (ErrorCode.OK != errorCode)
+            {
+                // TODO: Insert error message or telemetry status here
+            }
+            rightMotors[0].configSelectedFeedbackSensor(FEEDBACK_DEVICE, pidIndex, CAN_TIMEOUT_MSEC);
+            errorCode = rightMotors[0].configSelectedFeedbackSensor(FEEDBACK_DEVICE, pidIndex, CAN_TIMEOUT_MSEC);
+            if (ErrorCode.OK != errorCode)
+            {
+                // TODO: Insert error message or telemetry status here
+            }            
+        }
+
+        disable();
+    }
+
+    /**
+     * disable - put the robot in a known state
+     */
+    public void disable() 
+    {
+        drive.stopMotor();
+    }
+
+    @Override
+    public void initDefaultCommand() 
+    {
+        // Set the default command for a subsystem here.
+        // setDefaultCommand(new MySpecialCommand());
+    }
+
+    /**
+     * drive - simplified interface to that selects drive type based on driveStyleChoose
+     * @param speed
+     * @param turnOrRightSpeed
+     */
+    public void drive(Joystick control)     // TODO: Not sure I like this, may just want separate functions with speed/turn or left/right args
+    {
+        switch (driveStyleChooser.getSelected())
+        {
+            default:
+            case WPI_ARCADE:
+            {
+                double speed = control.getX(Hand.kRight);
+                double turn  = control.getY(Hand.kRight);
+                drive.arcadeDrive(speed, turn, false); // Don't square yet, will work on scaler later as new Joystick type
+                break;
+            }
+            case WPI_CURVATURE:
+            {
+                double speed = control.getX(Hand.kRight);
+                double turn  = control.getY(Hand.kRight);
+                drive.curvatureDrive(speed, turn, Math.abs(speed)<0.25); // Use quickturn logic when speed is "low"
+                break;
+            }
+            case WPI_TANK:
+            {
+                double leftSpeed = control.getX(Hand.kRight);
+                double rightSpeed = control.getZ();
+                drive.tankDrive(leftSpeed, rightSpeed);
+                break;
+            }
+            case MODIFIED_ARCADE:
+            {
+                break;
+            }
+        }
+    }
+
+    private void telemetry()
+    {
+        leftPosition_ticks = leftMotors[0].getSelectedSensorPosition(0);
+        leftVelocity_ticksPsec = leftMotors[0].getSelectedSensorVelocity(0)*10;
+
+        rightPosition_ticks = rightMotors[0].getSelectedSensorPosition(0);
+        rightVelocity_ticksPsec = rightMotors[0].getSelectedSensorVelocity(0)*10;
+
+        SmartDashboard.putNumber("leftPosition_ticks", leftPosition_ticks);
+        SmartDashboard.putNumber("leftVelocity_ticksPsec", leftVelocity_ticksPsec);
+        SmartDashboard.putNumber("rightPosition_ticks", rightPosition_ticks);
+        SmartDashboard.putNumber("rightVelocity_ticksPsec", rightVelocity_ticksPsec);
+
+        for (int i = 0; i < leftMotors.length; ++i) 
+        {
+            leftCurrent_amp[i] = leftMotors[i].getOutputCurrent();
+        }
+        for (int i = 0; i < rightMotors.length; ++i) 
+        {
+            rightCurrent_amp[i] = rightMotors[i].getOutputCurrent();
+        }
+        SmartDashboard.putNumberArray("leftCurrent_amp", leftCurrent_amp);
+        SmartDashboard.putNumberArray("rightCurrent_amp", rightCurrent_amp);
+    }
+
+    @Override
+    public void periodic() 
+    {
+        telemetryEnabled = SmartDashboard.getBoolean("DriveTelemetryEnabled",false);
+        if (telemetryEnabled)
+        {
+            telemetry();
+        }
+    }
+}
